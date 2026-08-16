1e1916a11bb1726cb2e3aa9f14b3f7fedc1a877e
prashantmishradevx-ctrl <prashantmishra.devx@gmail.com>
2026-08-16 14:58:31 +0530

    [100] [Time Beats: 100.00%] [Memory Beats: 59.61%] - LeetPush

---
+++ 100-same-tree/100-Same-Tree.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSameTree(self, p, q):
+
+        if p is None and q is None:
+            return True
+
+        if p is None or q is None:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        left_same = self.isSameTree(p.left, q.left)
+        right_same = self.isSameTree(p.right, q.right)
+
+        return left_same and right_same
